e8d67c07d22928621cdc5d11e207e991f6772003
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-24 10:37:50 +0900

    circle 잠시 중단

--- character_grass.py
+++ character_grass.py
@@ -5,8 +5,30 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_top():
+    print('top')
+    pass
+          
+def run_right():
+    print('right')
+    pass
+
+def run_bottom():
+    print('bottom')
+    pass
+
+def run_left():
+    print('left')
+    pass
+
+
 def run_rectangle():
     print('rectangle')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
+    
     pass
 
 def run_circle():
@@ -21,12 +43,12 @@ def run_circle():
         clear_canvas_now()
         boy.draw_now(x,y)
 
-        delay(0.1)
+        delay(0.01)
     pass
 
 while True:
 
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
