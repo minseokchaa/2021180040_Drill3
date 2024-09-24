068acfb516d403157ae8ae8c9f77ef39d69ceb0b
minseokcha@laptop <alstjr0526@gmail.com>
2024-09-24 09:49:53 +0900

    뼈대 구조 완성
    사각형 운동
    원 운동
    
    무한루프 구조 완성
     사각형 운동
     원 운동

--- character_grass.py
+++ character_grass.py
@@ -1,16 +1,20 @@
 from pico2d import*
 
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
 x=0
-while(x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x=x+2
-    delay(0.01)
+while(TRUE):
+
+    run_rectangle()
+    run_circle()
 
 
 
